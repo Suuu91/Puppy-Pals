6d5c1e133cdb7656cce562206fe28f54155d210e
zhejun su <suzhejunk9@gmail.com>
2024-10-28 21:16:05 -0400

    render puppy names

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
-import './App.css'
+import {puppyList} from './data.js' 
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log("puppyList:", puppyList)
   return (
-    <>
-    </>
+    <div className = "App">
+      { 
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>;
+        })
+      }
+    </div>
   )
 }
 
